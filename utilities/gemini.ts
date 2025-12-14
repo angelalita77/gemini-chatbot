0cdd639830238a53f28e9743f4c247e05c598bc5
angelalita77 <anjeanette@gmail.com>
2025-12-13 23:30:26 -0500

    Need to fix geminiApiKey being undefined

--- utilities/gemini.ts
+++ utilities/gemini.ts
@@ -1,6 +1,22 @@
+import { GoogleGenerativeAI } from '@google/generative-ai';
 import dotenv from 'dotenv'; 
 
 dotenv.config();
 const geminiApiKey = process.env.VITE_Gemini_API_Key;
 
 
+const genAI = new GoogleGenerativeAI(geminiApiKey);
+
+
+
+// const generationConfig = {
+//     temperature: 1,
+//     topP: 0.95,
+//     topK: 64,
+//     response_mime_type: 'application/json'
+// };
+
+// const model = genAI.getGenerativeModel({
+//     model: 'gemini-1/5-flash',
+//     generationConfig
+// });
